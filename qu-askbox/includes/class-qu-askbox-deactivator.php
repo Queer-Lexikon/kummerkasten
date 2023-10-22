298c41cf8858f60677a1dd881d5bff271062d139
Florian Palme <info@florian-palme.de>
2023-10-22 17:26:01 +0200

    first rewrite of the plugin
    started to rewrite the base code, based on the plugin boilerplate from https://github.com/DevinVinson/WordPress-Plugin-Boilerplate

---
+++ qu-askbox/includes/class-qu-askbox-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/Queer-Lexikon/kummerkasten
+ * @since      2.0.0
+ *
+ * @package    Qu_Askbox
+ * @subpackage Qu_Askbox/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      2.0.0
+ * @package    Qu_Askbox
+ * @subpackage Qu_Askbox/includes
+
+ */
+class Qu_Askbox_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    2.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ qu-askbox/includes/class-qu-askbox.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://github.com/Queer-Lexikon/kummerkasten
+ * @since      2.0.0
+ *
+ * @package    QU_Askbox
+ * @subpackage QU_Askbox/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      2.0.0
+ * @package    QU_Askbox
+ * @subpackage QU_Askbox/includes
+ */
+class QU_Askbox {
+
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      Qu_Askbox_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+    /**
+     * @since   2.0.0
+     * @access  protected
+     * @var     QU_Askbox_Public_Shortcode_Askbox   $shortcode_askbox   The shortcode [askbox]
+     */
+    protected $shortcode_askbox;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    2.0.0
+	 */
+	public function __construct() {
+		if ( defined( 'QU_ASKBOX_VERSION' ) ) {
+			$this->version = QU_ASKBOX_VERSION;
+		} else {
+			$this->version = '2.0.0';
+		}
+		$this->plugin_name = 'qu-askbox';
+
+		$this->load_dependencies();
+		$this->set_locale();
+
+        // currently not using any admin stuff
+		// $this->define_admin_hooks();
+
+		$this->define_public_hooks();
+
+	}
+
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Qu_Askbox_Loader. Orchestrates the hooks of the plugin.
+	 * - Qu_Askbox_i18n. Defines internationalization functionality.
+	 * - Qu_Askbox_Admin. Defines all hooks for the admin area.
+	 * - Qu_Askbox_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 */
+	private function load_dependencies() {
+
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-qu-askbox-loader.php';
+
+		/**
+		 * The class responsible for defining internationalization functionality
+		 * of the plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-qu-askbox-i18n.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the admin area.
+		 */
+		//require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-qu-askbox-admin.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the public-facing
+		 * side of the site.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-qu-askbox-public.php';
+
+        /**
+         * Needed Shortcode classes
+         */
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/shortcode/class-qu-askbox-public-shortcode-askbox.php';
+
+		$this->loader = new Qu_Askbox_Loader();
+        $this->shortcode_askbox = new QU_Askbox_Public_Shortcode_Askbox();
+	}
+
+	/**
+	 * Define the locale for this plugin for internationalization.
+	 *
+	 * Uses the Qu_Askbox_i18n class in order to set the domain and to register the hook
+	 * with WordPress.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 */
+	private function set_locale() {
+
+		$plugin_i18n = new Qu_Askbox_i18n();
+
+		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+
+	}
+
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 */
+	private function define_admin_hooks() {
+
+		$plugin_admin = new Qu_Askbox_Admin( $this->get_plugin_name(), $this->get_version() );
+
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+
+	}
+
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 */
+	private function define_public_hooks() {
+
+		//$plugin_public = new Qu_Askbox_Public( $this->get_plugin_name(), $this->get_version() );
+
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+
+        $this->loader->add_shortcode('askbox', [$this->shortcode_askbox, 'run'] );
+
+	}
+
+
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    2.0.0
+	 */
+	public function run() {
+		$this->loader->run();
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     2.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     2.0.0
+	 * @return    Qu_Askbox_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader() {
+		return $this->loader;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     2.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+
+}
